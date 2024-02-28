c61065ce2424aa41fb8bdd8860521336943da401
Christof Röhl <userpattern@gmail.com>
2024-02-29 00:03:20 +0100

    started refactoring and changing lost of stuff

---
+++ src/components/AlienData/AlienData.js
@@ -0,0 +1,53 @@
+export const initialProgress = [
+  { id: 1, name: "Aliens", count: 2, emoji: "👾" },
+  { id: 2, name: "Crystal", count: 0, emoji: "💎" },
+  { id: 3, name: "Seeds", count: 10, emoji: "🫘" },
+  { id: 4, name: "Plants", count: 0, emoji: "🌱" },
+];
+
+export const levels = [
+  { level: 1, name: "Crystal Hill", crystal: 2000 },
+  { level: 2, name: "Crystal Mountain", crystal: 5000 },
+  { level: 3, name: "Dig Deeper", crystal: 12000 },
+];
+
+export const randomMessages = [
+  "You make us mine!",
+  "What a lovely day to plant!",
+  "Planet Claire has pink air!",
+  "Oxygen included! (Also poisonous gas ...)",
+  "I love me a good pickaxe to mine more Crystal!",
+  "Plants, yummy!",
+  "The aliens are happy!",
+  "This is our planet. Nice, huh?",
+  "All our base are belong to us!",
+  "You say we have a problem?",
+  "DARKNESS!",
+  "Life just doesn't work without Crystal!",
+  "We need more aliens!",
+  "NASA won't come up here. Too far for their inferior technology.",
+  "The aliens are happy!",
+  "Planet Claire has pink air!",
+];
+
+export const shopStock = [
+  { id: 1, name: "Pickaxe", price: 42, message: "I picked this." },
+  { id: 2, name: "Hammer", price: 200, message: "Hammertime!" },
+  {
+    id: 3,
+    name: "Power drill",
+    price: 200,
+    message: "Now we're drilling property!",
+  },
+  { id: 4, name: "Small rocket", price: 2000, message: "It's rocket science!" },
+];
+
+// Vermutlich obsolet
+const LevelTwo = [
+  { id: 1, name: "Level", count: 1 },
+  { id: 2, name: "aliens", count: 2 },
+  { id: 3, name: "crystal", count: 0 },
+  { id: 4, name: "crystalToMine", count: 5000 },
+  { id: 5, name: "seeds", count: 10 },
+  { id: 6, name: "plants", count: 0 },
+];
--- src/components/AlienPrototype/AlienProtopye.js
+++ src/components/AlienPrototype/AlienProtopye.js
@@ -1,11 +1,9 @@
+import { useEffect, useState } from "react";
 import useLocalStorageState from "use-local-storage-state";
 
-const initialProgress = [
-  { id: 0, name: "aliens", count: 2 },
-  { id: 1, name: "crystal", count: 0 },
-  { id: 2, name: "crystalToMine", count: 2000 },
-  { id: 3, name: "seeds", count: 10 },
-];
+import { initialProgress, levels } from "../AlienData/AlienData";
+import { shopStock } from "../AlienData/AlienData";
+import { randomMessages } from "../AlienData/AlienData";
 
 function alienBreeding(a) {
   if (a <= 100) {
@@ -14,11 +12,184 @@ function alienBreeding(a) {
   return Math.round(a * 1.03);
 }
 
+function BuyButton({
+  onClick,
+  shopStock,
+  product,
+  progress,
+  setProgress,
+  setMessage,
+}) {
+  return (
+    <button
+      onClick={(price) => {
+        if (progress.crystal >= price) {
+          const newProgress = {
+            ...progress,
+            crystal: progress.crystal - price,
+            ...(product === "pickaxe"
+              ? { pickaxes: progress.pickaxes + 1 }
+              : {}),
+            ...(product === "smallRocket"
+              ? { spaceships: progress.spaceships + 1 }
+              : {}),
+          };
+          setProgress(newProgress);
+          setMessage("Bought something, yeah!");
+        } else {
+          console.log("no deal");
+          setProgress(progress);
+          setMessage("We don't have enough crystal to buy this!");
+        }
+      }}
+    >
+      BUY!
+    </button>
+  );
+}
+
 export default function AlienPrototype() {
   const [progress, setProgress] = useLocalStorageState("progress", {
     defaultValue: initialProgress,
   });
-  //   const newProgress = progress.map((prog) => prog.id === prog.id ?  {...progress, count: alienBreeding(prog.count)});
-  console.log(progress);
-  return <p>{alienBreeding(2)}</p>;
+  const [message, setMessage] = useState("Press start to play!");
+  const [crystalToMine, setCrystalToMine] = useLocalStorageState(
+    "crystalToMine",
+    { defaultValue: 2000 }
+  );
+
+  function handleClick() {
+    function getRandom() {
+      return Math.floor(Math.random() * randomMessages.length);
+    }
+    const randomMessage = randomMessages[getRandom()];
+    setMessage(randomMessage);
+
+    setProgress(
+      progress.map((progres) => ({ ...progres, count: progres.count++ }))
+    );
+  }
+
+  function handleStart() {
+    setMessage("Game started!");
+  }
+
+  function handleBreed() {
+    setProgress(
+      progress.map((progres) =>
+        progres.name === "Aliens"
+          ? { ...progres, count: progres.count * 2 }
+          : progres
+      )
+    );
+  }
+  function handleMine() {
+    let mineCrystal = Math.round(progress[0].count / 4);
+
+    if (crystalToMine === 0) {
+      mineCrystal = 0;
+      setMessage("No Crystal left to mine!");
+    } else if (crystalToMine <= mineCrystal) {
+      mineCrystal = crystalToMine;
+      setMessage("Let the pickaxes go, our work is done her!");
+    } else {
+      mineCrystal = Math.round(progress[0].count / 4);
+      setMessage("We need more Crystal!");
+    }
+
+    setCrystalToMine(Math.max(0, crystalToMine - mineCrystal));
+    setProgress(
+      progress.map((progres) =>
+        progres.name === "Crystal"
+          ? { ...progres, count: progres.count + mineCrystal }
+          : progres
+      )
+    );
+  }
+  // useEffect(() => {
+  //   handleStart();
+  // }, []);
+
+  // console.log(progress);
+  return (
+    <section>
+      <h1>My cute aliens</h1>
+      <p>(ALPHA TEST!!!)</p>
+
+      <p>For testing </p>
+      <button onClick={handleBreed}>BREED Fast!</button>
+      <button onClick={handleMine}>MINE CRYSTAL!</button>
+      {/* <button onClick={handleFarm}>FARM!</button>
+      <button onClick={handleDeath}>GET OLD AND DIE!</button> */}
+
+      <h2>
+        - Level {levels[0].level} - Crystal Hill: {crystalToMine} Crystal{}-
+      </h2>
+      <button onClick={handleStart}>START GAME!</button>
+
+      {progress.map((progres) => (
+        <p key={progres.id}>
+          <span>{progres.emoji}</span>
+          {progres.name}: {progres.count}
+        </p>
+      ))}
+
+      <h3>Message: {message}</h3>
+
+      <h3>Shop</h3>
+      <ul>
+        <li>
+          ⛏️ Pickaxe: 100 Crystal
+          <BuyButton price={shopStock[1].price} product="pickaxe">
+            BUY!
+          </BuyButton>
+        </li>
+        {/* <li>
+          ⚒️ Advanced Tools: {prices.advancedTools} Crystal
+          <BuyButton price={prices.advancedTools} product="advancedTools">
+            BUY!
+          </BuyButton>
+        </li>
+        <li>
+          🚀 Small Rocket: {prices.smallrocket} Crystal
+          <BuyButton price={prices.smallrocket} product="smallRocket">
+            BUY!
+          </BuyButton>
+        </li> */}
+      </ul>
+      <br></br>
+      <p>
+        <button className="main-button" onClick={handleClick}>
+          CLICK ME!
+        </button>
+      </p>
+      <p>
+        <button
+          onClick={() => {
+            setProgress(initialProgress);
+            setCrystalToMine(2000);
+          }}
+        >
+          RESET
+        </button>
+        {/* <button onClick={() => setProgress(LevelTwo)}>LEVEL 2</button> */}
+      </p>
+      {/* {progress.aliens >= 100 ? (
+    <span>
+      👾👾👾👾👾👾👾👾👾👾<br></br>
+      👾👾👾👾👾👾👾👾👾👾<br></br>
+      👾👾👾👾👾👾👾👾👾👾<br></br>
+      👾👾👾👾👾👾👾👾👾👾<br></br>
+      👾👾👾👾👾👾👾👾👾👾<br></br>
+      👾👾👾👾👾👾👾👾👾👾<br></br>
+      👾👾👾👾👾👾👾👾👾👾<br></br>
+      👾👾👾👾👾👾👾👾👾👾<br></br>
+      👾👾👾👾👾👾👾👾👾👾<br></br>
+      👾👾👾👾👾👾👾👾👾👾<br></br>
+    </span>
+  ) : (
+    <p>👾👾</p>
+  )} */}
+    </section>
+  );
 }
